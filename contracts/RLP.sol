6489f3a22634201e054a4c042ff7bba0456c774a
Hamdi Allam <hamdi.allam97@gmail.com>
2018-05-29 00:18:55 -0700

    scraping the encoder. Building the decoder

--- contracts/RLP.sol
+++ contracts/RLP.sol
@@ -1,60 +1,126 @@
 pragma solidity ^0.4.24;
 
 library RLP {
-    uint8 constant STRING_START = 0x80;
-    uint8 constant STRING_LONG_START = 0xb8;
+    uint8 constant STRING_SHORT_START = 0x80;
+    uint8 constant STRING_LONG_START  = 0xb8;
+    uint8 constant LIST_SHORT_START   = 0xc0;
+    uint8 constant LIST_LONG_START    = 0xf8;
 
     uint8 constant WORD_SIZE = 32;
 
     /*
-    *   Encoding
+    *   Decoder
     */
 
-    function encodeItem(bytes memory data) public pure returns (bytes memory result) {
-
-        uint256 len = data.length;
-        uint data_start;
-        assembly {
-            data_start := add(data, 0x20) // first byte is the length
-
-            result := mload(0x40) // free memory
-        }
-        
-        if (len == 1)
-        {
-            uint8 temp;
-            assembly { // retrieve the value of this byte
-                temp := mload(add(data, 0x20))
-            }
-
-            if (temp < STRING_START)
-                return data;
-            // if temp == STRING_START, flow into the next if condition
-        }
-
-        if (len < STRING_LONG_START)
-        {
-            uint result_start;
-            assembly {
-                mstore(0x40, add(result, and(add(add(len, 0x21), 0x1f), not(0x1f)))) // padding
-                mstore(result, add(len, 1)) // store the correct length
-                mstore8(add(result, 0x20), add(len, 0x80))
-
-                result_start := add(result, 0x21)
-            }
-            
-            copy(data_start, result_start, len);
-        }
-
-        else
-        {
-            assembly {
-                mstore(0x40, add(result, and(add(add(len, 0x40), 0x1f), not(0x1f)))) // padding
-            }
-        }
-
+    struct RLPItem {
+        uint len;
+        uint memPtr;
     }
 
+    struct Iterator {
+        uint itemsLeft;
+        uint nextPtr;
+        RLPItem currItem;
+    }
+
+    /*
+    * @param item RLP encoded bytes
+    */
+    function toRlpItem(bytes memory item) internal pure returns (RLPItem memory) {
+        if (item.length == 0) 
+            return RLPItem(0, 0);
+
+        uint memPtr;
+        assembly {
+            memPtr := add(item, 0x20)
+        }
+
+        return RLPItem(item.length, memPtr);
+    }
+
+    /*
+    * @param item RLP encoded list
+    */
+    function toList(RLPItem memory item) internal pure returns (RLPItem[] memory result) {
+        // check if it is a list
+        require(isList(item));
+
+        uint numItems = 0;
+        result = new RLPItem[](numItems);
+        for (uint i = 0; i < numItems; i++) {
+        }
+    }
+
+
+    /*
+    * Helpers
+    */
+
+    function isList(RLPItem memory item) internal pure returns (bool) {
+        uint len;
+        uint memPtr = item.memPtr;
+        assembly {
+            len := byte(0, mload(memPtr))
+        }
+
+        if (len < LIST_SHORT_START)
+            return false;
+        return true;
+    }
+
+    // @return rlp item byte length
+    function itemLength(RLPItem memory item) internal pure returns (uint) {
+        uint len;
+        uint memPtr = item.memPtr;
+        assembly {
+            len := byte(0, mload(memPtr))
+        }
+
+        if (len < STRING_SHORT_START)
+            return 1
+        
+        else if (len < STRING_LONG_START)
+            return len - STRING_SHORT_START + 1
+
+        else if (len < LIST_SHORT_START) {
+            assembly {
+                len := sub(len, 0xb7) // # of bytes the actual length is
+                memPtr := add(memPtr, 1) // skip over the first byte
+                
+                /* 32 byte word size */
+                let data_len := div(mload(memPtr), exp(256, sub(32, len))) // right shifting to the correct length
+                len := add(data_len, add(len, 1))
+            }
+            return len;
+        }
+
+        else if (len < LIST_LONG_START) {
+            return len - LIST_SHORT_START + 1;
+        } 
+
+        else {
+            assembly {
+                len := sub(len, 0xf7)
+                memPtr := add(memPtr, 1)
+
+                data_len := div(mload(memPtr), exp(256, sub(32, len))) // right shifting to the correct length
+                len := add(data_len, add(len, 1))
+            }
+            return len;
+        }
+    }
+
+    function numItems(RLPItem memory items) internal pure returns (uint) {
+    }
+
+    function _validate(RLPItem memory item) internal pure returns (bool) {
+    }
+
+    /*
+    * @param src Pointer to source
+    * @param dest Pointer to destination
+    * @param len Amount of memory to copy from the source
+    */
     function copy(uint src, uint dest, uint len) internal pure {
         // copy as many word sizes as possible
         for (; len >= WORD_SIZE; len -= WORD_SIZE) {
@@ -74,8 +140,4 @@ library RLP {
             mstore(dest, or(destpart, srcpart))
         }
     }
-
-    /*
-    *   Decoding
-    */
 }
--- test/test.js
+++ test/test.js
@@ -10,20 +10,4 @@ contract('RLP', async () => {
     before(async () => {
         rlp = await rlp.deployed();
     });
-
-    it('properly encodes single byte item less than 0x7f', async () => {
-        item = 0x10;
-        encodedItem = toHex(RLP.encode(item));
-
-        result = await rlp.encodeItem.call(item);
-        assert(result == encodedItem, "library incorrectly encoded single byte item");
-    });
-
-    it('properly encodes a string with length less than 0xb8', async () => {
-        item = 'hello world';
-        encodedItem = toHex(RLP.encode(item))
-
-        result = await rlp.encodeItem.call(item);
-        assert(result == encodedItem, "library incorrect encoded the string");
-    });
 });
