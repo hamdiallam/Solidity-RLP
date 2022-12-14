89d7839cf88ce515805fb49fbadf6f54948648d9
Hamdi Allam <hamdi.allam97@gmail.com>
2022-12-13 20:51:48 -0800

    max 0.8.17 single line function modifiers/return. specify solc version in truffle config

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-pragma solidity >=0.5.0 <0.7.0;
+pragma solidity >=0.5.0 <0.8.17;
 
 contract Migrations {
   address public owner;
@@ -10,7 +10,7 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-  constructor() public {
+  constructor() {
     owner = msg.sender;
   }
 
